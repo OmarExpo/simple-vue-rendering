f02ff08b9168fb86d93ca0ccdc6d647b3d501a39
unknown <omar1807@stud.kea.dk>
2022-08-19 16:09:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue-rendering
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/omarexpo/pen/yLKZJaj](https://codepen.io/omarexpo/pen/yLKZJaj).
+
+Vue rendering data object practice - using vue contsructor only.
---
+++ dist/script.js
@@ -0,0 +1,11 @@
+var data = {
+ "name": "Bamboo Thermal Ski Coat",
+ "description": "You'll be the most watched on YouTube",
+ "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG8ZI0HvjQtONlSn5BApUzlZ7SbbNryEk0VP_gwybdAw&s",
+ "price": 199
+}
+
+var app = new Vue({
+ el: "#app",
+ data: data
+})
